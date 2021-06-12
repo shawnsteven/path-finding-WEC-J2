e456fd9c946237ff5807cf9154fe3d93db50282f
ShawnSteven <shawnsteven405@gmail.com>
2021-06-12 10:29:28 -0400

    Initial Setup

--- PathFinding.py
+++ PathFinding.py
@@ -1,1 +1,10 @@
 # WEC Competition - Rhys, Anh, Shawn
+
+def path_Finding(dimension, botCoord, userCoord, obstacleArray, itemCoord):
+
+    reachedObjective = False
+
+    while(not reachedObjective):
+        
+
+print(path_Finding())
