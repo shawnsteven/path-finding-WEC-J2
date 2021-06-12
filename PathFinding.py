79c356659c46524da3b8d44710abf7ac585060bd
rhyscornelious <r4cornel@uwaterloo.ca>
2021-06-12 10:38:06 -0400

    Output function

--- PathFinding.py
+++ PathFinding.py
@@ -1,10 +1,21 @@
 # WEC Competition - Rhys, Anh, Shawn
 
+
 def path_Finding(dimension, botCoord, userCoord, obstacleArray, itemCoord):
 
     reachedObjective = False
 
     while(not reachedObjective):
-        
+
+    outputCoords(botCoord, numMoves, isComplete)
+
+
+def outputCoords(botCoord, numMoves, isComplete):
+    newOutput = (botCoord.x + " " + botCoord.y + ", ")
+    finalLine = finalLine + newOutput
+    if(isComplete):
+        print(finalLine)
+        print(numMoves)
+
 
 print(path_Finding())
