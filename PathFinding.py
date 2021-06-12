a4ba5eb7dbb5aee65c4913f7bc260070bbc56a71
rhyscornelious <r4cornel@uwaterloo.ca>
2021-06-12 12:22:35 -0400

    pp

--- PathFinding.py
+++ PathFinding.py
@@ -107,4 +107,4 @@ def path_Finding(dimension, botCoord, userCoord, obstacleArray, itemCoord):
     outputCoords(botPath, numMoves)
 
 
-path_Finding("3 3", "0 0", "2 2", " ", " ")
+path_Finding("3 3", "0 0", "1 2", "0 1 0 2", " ")
